3e1216e1b305822bf06b0a5786c874f80679c19b
rgap <r.guzmanap@gmail.com>
2025-03-28 23:33:09 -0500

    script tag and modules

---
+++ 26_script_and_modules/2_external_script/1_script/script.js
@@ -0,0 +1,5 @@
+// Access the DOM element now that it is available
+var element = document.getElementById("myElement");
+
+// Log the content of the element to the console
+console.log("Element from external script:", element.innerHTML); // Expected output: "Hello from the DOM!"
---
+++ 26_script_and_modules/2_external_script/2_script_and_style/script.js
@@ -0,0 +1,4 @@
+// Now that the DOM is fully loaded, we can safely access the element.
+var element = document.getElementById("myElement");
+// Log the element's content to the console.
+console.log("Element from external script:", element.innerHTML); // Expected output: "Hello from the DOM!"
